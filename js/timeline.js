e17e0fe72c3e718a7c9369d38abb4dc7ffeccb5f
Abrar0604 <abrar2004myself@gmail.com>
2024-09-23 10:11:35 +0530

    minor changes

--- js/timeline.js
+++ js/timeline.js
@@ -2,32 +2,34 @@
     "use strict";
 
     var items = document.querySelectorAll(".timeline li");
-  
 
     function isElementInViewport(el) {
-      var rect = el.getBoundingClientRect();
-      return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <=
-          (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-      );
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
     }
-  
+
     function callbackFunc() {
-      for (var i = 0; i < items.length; i++) {
-        if (isElementInViewport(items[i])) {
-          items[i].classList.add("in-view");
+        for (var i = 0; i < items.length; i++) {
+            if (isElementInViewport(items[i])) {
+                // Add the "in-view" class when the element is visible
+                items[i].classList.add("in-view");
+            } else {
+                // Remove the "in-view" class when the element is not visible
+                items[i].classList.remove("in-view");
+            }
         }
-      }
     }
-  
-    // listen for events
+
+    // Listen for events
     window.addEventListener("load", callbackFunc);
     window.addEventListener("resize", callbackFunc);
     window.addEventListener("scroll", callbackFunc);
-  })();
+})();
   (function () {
     "use strict";
 
